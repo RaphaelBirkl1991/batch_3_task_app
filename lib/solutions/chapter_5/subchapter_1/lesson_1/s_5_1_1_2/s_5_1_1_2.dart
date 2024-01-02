e4dc727419abeb49aed3ae1df6eb0f722e2d04bd
raphael <raphael.birkl@gmail.com>
2024-01-02 13:42:47 +0100

    5.1.1.2 hottest lama

--- lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2_styles.dart';
 
 class S5112 extends StatelessWidget {
   const S5112({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(children: [
+      Image.asset(
+        imagePathHottestLama,
+        width: 300,
+        height: 300,
+      ),
+      Text("Name", style: labelStyle),
+      Text("Diana das Lama", style: propertyStyle),
+      columnSpacing,
+      Text("Gewicht", style: labelStyle),
+      Text("120 kg", style: propertyStyle),
+      columnSpacing,
+      Text("Größe", style: labelStyle),
+      Text("1,80", style: propertyStyle),
+      columnSpacing,
+      Text("Lieblingsessen", style: labelStyle),
+      Text("Gras", style: propertyStyle)
+    ]);
   }
 }
---
+++ lib/solutions/chapter_5/subchapter_1/lesson_1/s_5_1_1_2/s_5_1_1_2_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const String imagePathHottestLama = "assets/images/tasks/lama.png";
+const labelStyle =
+    TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black);
+
+const propertyStyle =
+    TextStyle(fontSize: 16, fontWeight: FontWeight.normal, color: Colors.black);
+
+const columnSpacing = SizedBox(height: 30);
