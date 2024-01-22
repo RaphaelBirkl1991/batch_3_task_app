88a8da17e7137007bf9b7c084b6d640179dbcb5e
raphael <raphael.birkl@gmail.com>
2024-01-22 16:43:53 +0100

    5.4.1.1

--- lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
+++ lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
@@ -4,6 +4,26 @@ class S5411 extends StatelessWidget {
   const S5411({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          const snackbar = SnackBar(
+            content: Text("Aua Fehler"),
+          );
+          try {
+            throw CustomRuntimeException("Aua Fehler");
+          } catch (e) {
+            print("auauauau fehler o.O");
+            ScaffoldMessenger.of(context).showSnackBar(snackbar);
+          }
+        },
+        child: const Text("click here"),
+      ),
+    );
   }
+}
+
+class CustomRuntimeException implements Exception {
+  final String message;
+  CustomRuntimeException(this.message);
 }
